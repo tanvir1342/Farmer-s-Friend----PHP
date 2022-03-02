6b6d90d3e3d81f4154ca17751eb205334e47ba06
tanvir1342 <tanvirmahmud148@gmail.com>
2022-03-03 01:04:50 +0600

    excutive

--- Admin/Executive/Home.php
+++ Admin/Executive/Home.php
@@ -1,6 +1,46 @@
-<?php
+<!DOCTYPE html>
+<html>
+<!-- center table creation -->
+<table width = 100%;>
+    <tr height = 100px style ="background-color:#C1BCBC ">
+        <td width =10%; align = center>
+            <img width = 100px; height = 100px src ="logo.png">
+        </td>
+        <td align = right >
+            <table >
+                <!-- center menubar -->
+                <tr style ="font-size:20px;">
+                    <td><a href="#">Home  |</a></td>
+                    <td><a href="#"> About us  |</a></td>
+                    <td><a href="#"> logout</a></td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+    <!-- daynamic menubar -->
+    <tr  height = 700px>
+        <td width =10%; valign = top; style ="background-color:#C1BCBC; font-size:20px;">
+            <a href="#">create Account</a><br><br>
+            <a href="#">Edit Account</a><br><br>
+            <a href="#">Approve post</a><br><br>
+            <a href="#">Approve paper</a><br><br>
+            <a href="#">control deal</a><br><br>
+        </td>
+        <!-- output of user -->
+        <td valign = top style ="background-color:#F5F2F1 ">
 
-echo "hello world";
+            <h1 align = center>Welcome,GanDu</h1><hr>
 
 
-?>
+        </td>
+    </tr>
+    <!-- footer section -->
+    <tr  height = 100px;>
+        <td colspan="2" style ="background-color:black; color:white;align = center "; align = center>
+           coppyright @2022
+        </td>
+    </tr>
+</table>
+
+
+</html>
