84090c0a3d8552dec07bbc52ead18a7c9ddcf18e
Atto786 <rahatanjum1998@gmail.com>
2022-03-03 13:04:24 +0600

    1st

---
+++ Admin/Veterinarian/CheckRequest.php
@@ -0,0 +1,54 @@
+
+<html>
+<head>
+	<title>Check Request</title>
+</head>
+<body>
+
+<table border="1" width="100%">
+	<tr height="100px">
+		<td width="10%">Logo</td>
+		<td align="right">
+			<a href="homepage.php">Home</a>
+			<a href="0">| About Us</a>
+			<a href="0">| Logout</a>
+		</td>
+		
+
+	</tr>
+	<tr height="700px"> 
+		<td width="10%" valign="top">
+		<a href="CheckRequest.php">Check Farmer's Requests</a><br><br>
+		<a href="ApproveRequest.php">Approve Request</a><br><br>
+		<a href="MakeSchedule.php">Make Schedule For Treatment</a><br><br>
+		<a href="CheckHistory.php">Check History of treatment</a><br><br>
+		<a href="ProvidePDF.php">Provide PDF</a><br><br>
+		</td>
+		<td valign="top">
+			<h1 align="center">Check Farmer's Requests</h1><hr>
+			
+
+
+
+
+
+
+		</td>
+		
+
+	</tr>
+	<tr height="50px" align="center">
+		<td colspan="3"> Copyright@2022</td>
+		
+
+	</tr>
+
+
+
+</table>
+
+</body>
+
+
+
+</html>
