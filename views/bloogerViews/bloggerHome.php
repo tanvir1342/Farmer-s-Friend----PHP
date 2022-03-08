d1e7ac37bf9ae18bda4dca7e79dfd1c460b49b22
Atto786 <rahatanjum1998@gmail.com>
2022-03-08 22:33:54 +0600

    mvc1.1

---
+++ views/bloogerViews/bloggerHome.php
@@ -0,0 +1,54 @@
+
+<html>
+<head>
+	<title>Blogger</title>
+</head>
+<body>
+
+<table border="1" width="100%">
+	<tr height="100px">
+		<td width="10%">Logo</td>
+		<td align="right">
+			<a href="0">Home</a>
+			<a href="0">| About Us</a>
+			<a href="0">| Logout</a>
+		</td>
+		
+
+	</tr>
+	<tr height="700px"> 
+		<td width="10%" valign="top" align="center">
+		<a href="#">Create Account</a><br><br>
+		<a href="#">Edit Account</a><br><br>
+		<a href="#">Write a Post</a><br><br>
+		<a href="#">Share Link</a><br><br>
+		
+		</td>
+		<td valign="top">
+			<h1 align="center">Welcome To Blogger Dashboard</h1><hr>
+			
+
+
+
+
+
+
+		</td>
+		
+
+	</tr>
+	<tr height="50px" align="center">
+		<td colspan="3"> Copyright@2022</td>
+		
+
+	</tr>
+
+
+
+</table>
+
+</body>
+
+
+
+</html>
