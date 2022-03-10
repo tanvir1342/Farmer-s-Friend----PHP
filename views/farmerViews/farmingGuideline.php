8f17224d574d55e4e6cd7f1ef252854e04242765
Tomal Paul <tamal.paul.42123@gmail.com>
2022-03-10 23:41:55 +0600

    mvc 1.1

---
+++ views/farmerViews/farmingGuideline.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Table Creation -->
+<table width = 100%; border = 1px>
+    <tr height = 100px>
+        <td width= 10%; align = center>
+            <img width = 100px; height = 100px src = "logo.png">
+        </td> 
+        <!-- Top Menubar -->
+        <td align="right">
+            <table>
+                <tr>
+                    <td><a href="adviserAgro.php">Home|</a></td>
+                    <td><a href="#">  About US |</a></td>
+                    <td><a href="#">  Logout</a></td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+ <!-- Menubar of adviser -->
+    <tr height="700px">
+        <td width="10%"; valign="top">
+             
+            <!-- <a href="3.php">Post Request</a><br><br> -->
+            <a href="2. type selection.php">Farmer's Post</a><br><br>
+            <a href="6. content.php">Banking Content</a><br><br>
+            <a href="5. profile.php">Edit Profile</a><br>
+        </td>
+        <!-- Output -->
+        <td valign="top">
+            <!-- Farmers post -->
+            <h1 align="center">Farming Guideline</h1> <hr>
+            <form>  
+    <table>
+        <tr>
+            <td></td>
+            <td>Types Of Guideline</td>
+            <td>
+                <select>
+                    <option></option>
+                    <option>Plantation</option>
+                    <option>Husbandary</option>
+                    <option>Fisheries</option>
+                    <option>Others</option>
+                </select>
+            </td>
+
+        </tr>
+        <tr>
+            <td></td>
+        </tr>
+
+    </table>
+</form>
+
+            <h3>Topic Name : Plantation</h3>
+            <p>What’s in this guide to starting a small farm?<br>
+The state of small farm business in the BD.
+Why do you want to start a small farm?
+What to do if you’ve never farmed before<br>
+Step 1: Identify your niche<br>
+Step 2: Find the land<br>
+Step 3: Get financed<br>
+Step 4: Planning a farm for success<br>
+Step 5: Marketing your farm and products<br></p>
+            <a href="#">  Like |</a>
+            <a href="#">  Comment |</a>
+
+            <h3>Topic Name : Husbandary</h3>
+            <p>What’s in this guide to starting a small farm?<br>
+The state of small farm business in the BD.
+Why do you want to start a small farm?
+What to do if you’ve never farmed before<br>
+Step 1: Identify your niche<br>
+Step 2: Find the land<br>
+Step 3: Get financed<br>
+Step 4: Planning a farm for success<br>
+Step 5: Marketing your farm and products<br></p>
+            <a href="#">  Like |</a>
+            <a href="#">  Comment |</a>
+
+            <h3>Topic Name : Fisheries</h3>
+            <p>What’s in this guide to starting a small farm?<br>
+The state of small farm business in the BD.
+Why do you want to start a small farm?
+What to do if you’ve never farmed before<br>
+Step 1: Identify your niche<br>
+Step 2: Find the land<br>
+Step 3: Get financed<br>
+Step 4: Planning a farm for success<br>
+Step 5: Marketing your farm and products<br></p>
+            <a href="#">  Like |</a>
+            <a href="#">  Comment |</a>
+        </td>
+    </tr>
+    <!-- Footer Part -->
+    <tr height="100px">
+        <td colspan="2"; align="center">
+            All Copyrights Reserve by Gallant ltd.
+        </td>
+    </tr>
+
+</table>
+</html>
