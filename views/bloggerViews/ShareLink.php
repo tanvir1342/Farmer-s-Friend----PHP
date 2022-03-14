ca7a1387ab8afadfc3547cbf3992baf059bfde6d
Atto786 <rahatanjum1998@gmail.com>
2022-03-14 10:43:58 +0600

    mvc1.2

---
+++ views/bloggerViews/ShareLink.php
@@ -0,0 +1,56 @@
+
+<html>
+<head>
+	<title>Blogger</title>
+</head>
+<body>
+
+<table border="1" width="100%">
+	<tr height="100px">
+		<td width="10%">Logo</td>
+		<td align="right">
+			<a href="bloggerHome.php">Home</a>
+			<a href="0">| About Us</a>
+			<a href="0">| Logout</a>
+		</td>
+		
+
+	</tr>
+	<tr height="700px"> 
+		<td width="10%" valign="top" align="center">
+		
+		<a href="EditAccount.php">Edit Account</a><br><br>
+		<a href="WritePost.php">Write a Post</a><br><br>
+		<a href="ReadPost.php">Read Posts</a><br><br>
+		<a href="ShareLink.php">Share Content</a><br><br>
+		<a href="PublishResearchPaper.php">Research Paper</a><br><br>
+		
+		</td>
+		<td valign="top">
+			<h1 align="center">Share Link</h1><hr>
+			
+
+
+
+
+
+
+		</td>
+		
+
+	</tr>
+	<tr height="50px" align="center">
+		<td colspan="3"> Copyright@2022, Created By Team Gallant</td>
+		
+
+	</tr>
+
+
+
+</table>
+
+</body>
+
+
+
+</html>
---
+++ views/bloggerViews/WritePost.php
@@ -0,0 +1,71 @@
+
+<html>
+<head>
+	<title>Blogger</title>
+</head>
+<body>
+
+<table border="1" width="100%">
+	<tr height="100px" style="background-color:#C1BCBC">
+		<td width="10%" align="center">
+			<img width="100px" height="100px" src="logo.png">
+		</td>
+		<td align="right">
+			<a href="bloggerHome.php">Home</a>
+			<a href="0">| About Us</a>
+			<a href="0">| Logout</a>
+		</td>
+		
+
+	</tr>
+	<tr height="700px"> 
+		<td width="10%" valign="top" align="center" bgcolor="C1BCBC">
+		
+		<a href="EditAccount.php">Edit Account</a><br><br>
+		<a href="WritePost.php">Write a Post</a><br><br>
+		<a href="ReadPost.php">Read Posts</a><br><br>
+		<a href="PublishResearchPaper.php">Research Paper</a><br><br>
+		
+		</td>
+		<td valign="top">
+			<h1 align="center">Write content about farming</h1><hr>
+
+
+			<label for="fname"><b>Write a Post:</b></label><br>
+			<textarea rows="8" cols="100">
+				
+			</textarea><hr>
+			
+			<div class="clearfix">
+				<button type="button" class="cancelbtn">Cancel</button>
+				<button type="submit" class="signupbtn">Post</button>
+			</div><br><br>
+
+	
+
+
+
+
+
+
+
+
+		</td>
+		
+
+	</tr>
+	<tr height="50px" align="center">
+		<td colspan="3" style="background-color:black; color:white"> Copyright@2022, Created By Team Gallant</td>
+		
+
+	</tr>
+
+
+
+</table>
+
+</body>
+
+
+
+</html>
