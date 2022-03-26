5d5e3bf759fda822b760b19f33087b8d4bc52742
Mosaddek007 <hossainmdmosaddek101@gmail.com>
2022-03-26 13:13:22 +0600

    mvc56

--- controllers/farmerControllers/logincheck.php
+++ controllers/farmerControllers/logincheck.php
@@ -8,22 +8,6 @@ if(isset($_REQUEST['submit']))
 	$username = $_REQUEST['username'];
 	$password = $_REQUEST['password'];
 
-<<<<<<< HEAD
-	if($username != null && $password != null){
-		
-
-		$file = fopen('../../models/farmerlogin.txt', 'r');
-
-
-		while (!feof($file)) {
-			$user = fgets($file);
-			$abc = explode('|', $user);
-
-			if(trim($abc[2]) == $username && trim($abc[3]) == $password){
-				setcookie('status', 'true', time()+900, '/');
-				header('location: ../../views/farmerViews/farmerHome.php');
-			}
-=======
 	if($username != null && $password != null)
 	{
 		$status = login ($username,$password);
@@ -36,7 +20,7 @@ if(isset($_REQUEST['submit']))
 		else
 		{
 			header('location: ../../views/farmerViews/farmerHome.php');
->>>>>>> 6d766b814c8383f6a6a05dac1a731bdeac1e2e4b
+
 		}
 
 	}
