8cb6ece5668d8c965eb12599bf5c6c66027a4ba1
tanvir1342 <tanvirmahmud148@gmail.com>
2022-04-09 01:19:32 +0600

    css

--- views/AdminViews/Executive/createAdviser(eco).php
+++ views/AdminViews/Executive/createAdviser(eco).php
@@ -14,9 +14,147 @@
         }
     }
 ?>
+
+
 <!DOCTYPE html>
 <html>
-<!-- center table creation -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Adviser(Economic) user List</title>
+    <link rel="stylesheet" type="text/css" href="createUser.css">
+</head>
+<body>
+<div class="menubar">
+        <div class="menubar-icon">
+            <img src="image/logo.png">
+        </div>
+        <div class="menubar-link">
+            <a href="">Home |</a>
+            <a href="#"> About us |</a>
+            <a href="#"> Contact us |</a>
+            <a href="../../../controllers/adminControllers/excecutiveControllers/logout.php"> Logout </a>
+        </div>
+        
+    </div>
+    <div class="main_panle">
+       <!--  side panel div start from here -->
+       <div class="side_panel">
+             <div class="button_area_of_2nd_side_panel">
+            <button>User info</button>
+            <button>Control deal</button>
+            <button>Post chehck</button>
+            <button>Edit profile</button>
+
+
+        </div>
+       </div>
+      <!--  main panel daynamic area star from here -->
+        <div class="daynamic_area">
+        
+           <span align = center>Adviser(Economics) user List</span>
+            <table border="1px" align="center" width="80%" id="userTable">
+                <tr>
+                    <th>Name</th>
+                    <th>username</th>
+                    <th>email</th>
+                    <th>gender</td>
+                    <th>Age</th>
+                    <th>salary</th>
+                    <th>joining Date</th>
+                    <th>photo</th>
+                </tr>
+                <?php
+
+                if($user!=null){
+                foreach ($user as $user) {
+
+
+                    ?>
+
+                <tr>
+                    <td><?=$user['0']?></td>
+                    <td><?=$user['1']?></td>
+                    <td><?=$user['3']?></td>
+                    <td><?=$user['4']?></td>
+                    <td><?=$user['5']?></td>
+                    <td><?=$user['6']?></td>
+                    <td><?=$user['7']?></td>
+                    <td>null</td>
+                </tr>
+                <?php
+                    
+                }
+                }
+
+                ?>
+                
+            </table><br>
+
+        
+            <h3 align="center"><?=$msg?></h3><br>
+            <span align="center">Create new user</span><br>
+
+            <form method="POST" action="../../../controllers/adminControllers/excecutiveControllers/ecocreataccount.php">
+                <table class="userTable userTable_horizently" align="center">
+                    <tr>
+                        <td>Name</td>
+                        <td><input class="input_horizently" type="name" name="name" value=""></td>
+                    </tr>
+                    <tr>
+                        <td>Username</td>
+                        <td><input class="input_horizently" type="username" name="username" value=""></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input class="input_horizently" type="password" name="password" value=""></td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td><input class="input_horizently" type="email" name="email" value=""></td>
+                    </tr>
+                     <tr>
+                        <td>Gender</td>
+                        <td><input class="input_horizently" type="text" name="gender" value=""></td>
+                    </tr>
+                     <tr>
+                        <td>Age</td>
+                        <td><input class="input_horizently" type="text" name="age" value=""></td>
+                    </tr>
+                    <tr>
+                        <tr>
+                        <td>salary</td>
+                        <td><input class="input_horizently" type="number" name="salary" value=""></td>
+                    </tr>
+                    <tr>
+                        <td>joining date</td>
+                      
+                        <td><input class="input_horizently" type="date" name="join" value=""></td>
+                    </tr>
+                        <td colspan="2"><input class="submit_button" type="submit" name="submit" value="create user"></td>
+                    </tr>
+                    
+                </table>
+
+            </form>
+              
+        </div>
+
+        
+    </div>
+
+</body>
+</html>
+</body>
+</html>
+
+
+
+
+
+<!-- <!DOCTYPE html>
+<html>
+
 <table width = 100%;>
     <tr height = 100px style ="background-color:#C1BCBC ">
         <td width =10%; align = center>
@@ -24,7 +162,7 @@
         </td>
         <td align = right >
             <table >
-                <!-- center menubar -->
+              
                 <tr style ="font-size:20px;">
                     <td><a href="Home.php">Home  |</a></td>
                     <td><a href="#"> About us  |</a></td>
@@ -33,7 +171,7 @@
             </table>
         </td>
     </tr>
-    <!-- daynamic menubar -->
+  
     <tr  height = 700px>
         <td width =10%; valign = top; style ="background-color:#C1BCBC; font-size:20px;">
             <a href="listOfuser.php">user Account</a><br><br>
@@ -42,7 +180,7 @@
             <a href="approvePost.php">Approve post</a><br><br>
             <a href="EditProfile.php">Edit profile</a><br><br>
         </td>
-        <!-- output of user -->
+     
         <td valign = top style ="background-color:#F5F2F1 ">
 
             <h3 align = center>Adviser(Economics) user List</h3>
@@ -84,7 +222,7 @@
                 
             </table><br>
 
-            <!-- creat new user account  -->
+        
             <h3 align="center"><?=$msg?></h3><br>
             <h3 align="center">Create new user</h3><br>
 
@@ -134,7 +272,7 @@
 
         </td>
     </tr>
-    <!-- footer section -->
+
     <tr  height = 100px;>
         <td colspan="2" style ="background-color:black; color:white;align = center "; align = center>
            coppyright @2022
@@ -143,4 +281,4 @@
 </table>
 
 
-</html>
+</html> -->
